574aa7d06a91e5676e0871f0d45259ea413ee58d
ASlightlyOvergrownCactus <ASlightlyOvergrownCactus@gmail.com>
2023-03-31 16:02:00 -0500

    Added .lib file
    Forgor to add .lib file as well

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -11,7 +11,6 @@ using System.Security.Permissions;
 // IMPORTANT
 // This requires Fisobs to work!
 // Big thx to Dual-Iron (on github) for help with Fisobs!
-// Big thx to (insert discord name here if they are okay with it) for help with getting the collision to work!
 // This code was based off of Dual-Iron's Centishield as practice, I didn't make parts of this! (Probably add more details on that later)
 
 #pragma warning disable CS0618 // Do not remove the following line.
@@ -20,7 +19,7 @@ using System.Security.Permissions;
 namespace TestMod
     {
 
-// Add target_game_version and youtube_trailer_id to modinfo.json if applicable.
+
 // See https://rainworldmodding.miraheze.org/wiki/Downpour_Reference/Mod_Directories
 
 [BepInPlugin("cactus.testMod", "Test Mod - Crate", "0.1.0")]
@@ -28,35 +27,9 @@ sealed class Plugin : BaseUnityPlugin
 {
     public void OnEnable()
     {
-            // How to make a hook:
-            /*
-        On.RainWorld.OnModsInit += Init;
-
-            // Register hooks
-
-            On.Rock.ctor += Rock_ctor;
-            */
-
             Content.Register(new CrateFisobs());
     }
 
-
-
-        // Rock bounce thingy (DO NOT)
-    /*private void Rock_ctor(On.Rock.orig_ctor orig, Rock self, AbstractPhysicalObject abstractPhysicalObject, World world)
-        {
-            orig(self, abstractPhysicalObject, world);
-            self.gravity = 0.1f;
-            self.bounce = 3.0f;
-        }*/
-
-
-    private void Init(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-    {
-        orig(self);
-
-        Logger.LogDebug("Hello world!");
-    }
 }
 
 }
